ffc9ca6fa8311470e8094df40014930723d5252c
Bedirhan Yıldırım <github-yildirim.bedo08@gmail.com>
2026-06-15 18:44:45 +0300

    config.js ayrimi

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://wmsrmteyhpsjwvlwahnh.supabase.co";
 const SUPABASE_KEY = "sb_publishable_2kCfSVznVhIb4Q6l4-kQpQ_alQxfZNP";
-const PASSWORD = "ZB085355";
+const PASSWORD = "085355";
